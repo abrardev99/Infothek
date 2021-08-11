6e6a13b9bba4f870a6589839fb26b95bfd172208
Abrar Ahmad <abrar.dev99@gmail.com>
2021-08-11 20:43:43 +0500

    wip

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,9 +1,9 @@
 <div class="c-sidebar c-sidebar-dark c-sidebar-fixed c-sidebar-lg-show" id="sidebar">
     <div class="c-sidebar-brand d-lg-down-none">
-        <a href="{{ url('/') }}"><img src="{{ asset('frontend/images/logo.png') }}" class="c-sidebar-brand-full" width="118" height="46" alt="PromoBulls Logo"></a>
+        <a href="{{ url('/') }}"><img src="{{ asset('images/logo.png') }}" class="c-sidebar-brand-full" width="118" height="46" alt="{{ config('app.name') }} Logo"></a>
     </div>
     <ul class="c-sidebar-nav">
-        <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="{{ auth()->user()->dashboardUrl() }}">
+        <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="{{ route('dashboard') }}">
                 <svg class="c-sidebar-nav-icon">
                     <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-speedometer') }}"></use>
                 </svg>
