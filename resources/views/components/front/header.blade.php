a317019ad3de3c57c0166fd9853d3bb52fd7920e
Abrar Ahmad <abrar.dev99@gmail.com>
2021-08-13 23:39:47 +0500

    wip

--- resources/views/components/front/header.blade.php
+++ resources/views/components/front/header.blade.php
@@ -6,7 +6,7 @@
                 <form method="GET" action="{{ url('/') }}">
                     <div class="form">
                         <i class="fa fa-search"></i>
-                        <input value="{{ request()->get('q') }}" name="q" type="search" class="form-control form-input" placeholder="Search anything..."
+                        <input value="{{ request()->get('q') }}" name="q" type="search" class="form-control form-input" placeholder="Hit Enter to search"
                                autofocus>
                     </div>
                 </form>
